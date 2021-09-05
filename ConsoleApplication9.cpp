e6c0dd1f6ed7261fdd566723db60acc09adec5a5
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-09-05 16:24:02 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void katarn(int);
+
+int main()
+{
+	int x;
+
+	cout << "Press x: ";
+
+	cin >> x;
+
+	katarn(x);
+
+	return 0;
+}
+
+
+void katarn(int y)
+{
+	char d[40];
+
+	int i;
+
+	for (i = 0; y; i++)
+	{
+		d[i] = y % 2;
+
+		y /= 2; 
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		cout << (int)d[i];
+	}
+}
